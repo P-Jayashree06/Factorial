2a6299d75be6563e619a60dc8f176b6bc727acb2
P-Jayashree06 <jayashree122005@gmail.com>
2024-08-18 20:16:16 +0530

    Add files via upload

---
+++ Factorial_.java
@@ -0,0 +1,13 @@
+public class Factorial_ {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int fact= 1;
+        for (int i = 1; i >= n; i++) {
+            fact*= 1;
+
+        }
+        System.out.println(fact);
+
+    }
+}
